79d037fe17d2d1360e3ae15b32132878907bbeba
Tayfun Osman ALTAY <tayfunosman_0071@hotmail.com>
2024-02-19 16:58:12 +0300

    Shops namespace

---
+++ SrDictionary/Shops/CPackageItem.cs
@@ -0,0 +1,24 @@
+﻿using SrDictionary.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SrDictionary.Shops
+{
+    public sealed class PackageItem : Obj
+    {
+        public PackageItem() : base()
+        {
+            ExpandTerm = "EXPAND_TERM_ALL";
+        }
+        [SParam("@saletag", System.Data.SqlDbType.SmallInt)]
+        public short SaleTag { get; set; }
+        [SParam("@expandterm", System.Data.SqlDbType.VarChar, 129)]
+        public string? ExpandTerm { get; set; }
+        [SParam("@icon", System.Data.SqlDbType.VarChar, 129)]
+        public string? AssocFileIcon { get; set; }
+
+    }
+}
